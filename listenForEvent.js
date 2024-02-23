969ea9b6eab2df0b70ca2c697bfc09d799578455
Georgi Iliev <extern.Georgi.Iliev@aldi-sued.com>
2024-02-23 15:50:31 +0200

    ...

--- listenForEvent.js
+++ listenForEvent.js
@@ -11,7 +11,7 @@ async function run() {
         
         const eventEmitter = await client.subscribeFromReplayId(
             '/event/test_integration_ka__e',
-            1,
+            100,
             32674663
         );
         console.log('subscribed')
@@ -29,16 +29,16 @@ async function run() {
 
 run()
 
-[
-    '{"replayId":32673966,"payload":{"CreatedDate":1708523483486,"CreatedById":"0050600000FlTqqAAF","name__c":"ev 1"}}',
-    '{"replayId":32673967,"payload":{"CreatedDate":1708523483486,"CreatedById":"0050600000FlTqqAAF","name__c":"ev 2"}}',
-    '{"replayId":32674471,"payload":{"CreatedDate":1708526103429,"CreatedById":"0050600000FlTqqAAF","name__c":"Hello world"}}',
-    '{"replayId":32674525,"payload":{"CreatedDate":1708526525388,"CreatedById":"0050600000FlTqqAAF","name__c":"ev 2"}}',
-    '{"replayId":32674520,"payload":{"CreatedDate":1708526422134,"CreatedById":"0050600000FlTqqAAF","name__c":"ev 1"}}',
-    '{"replayId":32674663,"payload":{"CreatedDate":1708527479041,"CreatedById":"0050600000FlTqqAAF","name__c":"Test 1"}}',
-    '{"replayId":32674574,"payload":{"CreatedDate":1708526896972,"CreatedById":"0050600000FlTqqAAF","name__c":"Hello world"}}',
-    '{"replayId":32674521,"payload":{"CreatedDate":1708526422134,"CreatedById":"0050600000FlTqqAAF","name__c":"ev 2"}}',
-    '{"replayId":32674621,"payload":{"CreatedDate":1708527201616,"CreatedById":"0050600000FlTqqAAF","name__c":"Hello world"}}',
-    '{"replayId":32674589,"payload":{"CreatedDate":1708527015884,"CreatedById":"0050600000FlTqqAAF","name__c":"Hello world"}}',
-    '{"replayId":32674524,"payload":{"CreatedDate":1708526525388,"CreatedById":"0050600000FlTqqAAF","name__c":"ev 1"}}'
-  ]
+// [
+//     '{"replayId":32673966,"payload":{"CreatedDate":1708523483486,"CreatedById":"0050600000FlTqqAAF","name__c":"ev 1"}}',
+//     '{"replayId":32673967,"payload":{"CreatedDate":1708523483486,"CreatedById":"0050600000FlTqqAAF","name__c":"ev 2"}}',
+//     '{"replayId":32674471,"payload":{"CreatedDate":1708526103429,"CreatedById":"0050600000FlTqqAAF","name__c":"Hello world"}}',
+//     '{"replayId":32674525,"payload":{"CreatedDate":1708526525388,"CreatedById":"0050600000FlTqqAAF","name__c":"ev 2"}}',
+//     '{"replayId":32674520,"payload":{"CreatedDate":1708526422134,"CreatedById":"0050600000FlTqqAAF","name__c":"ev 1"}}',
+//     '{"replayId":32674663,"payload":{"CreatedDate":1708527479041,"CreatedById":"0050600000FlTqqAAF","name__c":"Test 1"}}',
+//     '{"replayId":32674574,"payload":{"CreatedDate":1708526896972,"CreatedById":"0050600000FlTqqAAF","name__c":"Hello world"}}',
+//     '{"replayId":32674521,"payload":{"CreatedDate":1708526422134,"CreatedById":"0050600000FlTqqAAF","name__c":"ev 2"}}',
+//     '{"replayId":32674621,"payload":{"CreatedDate":1708527201616,"CreatedById":"0050600000FlTqqAAF","name__c":"Hello world"}}',
+//     '{"replayId":32674589,"payload":{"CreatedDate":1708527015884,"CreatedById":"0050600000FlTqqAAF","name__c":"Hello world"}}',
+//     '{"replayId":32674524,"payload":{"CreatedDate":1708526525388,"CreatedById":"0050600000FlTqqAAF","name__c":"ev 1"}}'
+//   ]
--- sendEvent.js
+++ sendEvent.js
@@ -7,7 +7,7 @@ async function run() {
         const payload = {
             CreatedDate: new Date().getTime(), // Non-null value required but there's no validity check performed on this field
             CreatedById: '0050600000FlTqqAAF', // Valid user ID
-            name__c: { string: 'Test 3' } // Field is nullable so we need to specify the 'string' type
+            name__c: { string: 'Test 77' } // Field is nullable so we need to specify the 'string' type
         };
         const publishResult = await client.publish('/event/test_integration_ka__e', payload);
         console.log('Published event: ', JSON.stringify(publishResult));
